2d072704666ba23f3b6f9d05d4682ab99b7f111c
salahzadakamran1122-source <salahzadakamran1122@gmail.com>
2026-09-16 14:57:44 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
-const CACHE_NAME = 'diary-v1';
+const CACHE_NAME = 'diary-v2';
 const assets = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
   );
 });
 
